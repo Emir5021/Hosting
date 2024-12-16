5a95287e4a8a991942393cf207ba6fb8bc472e29
Emir5021 <je.rodriguez22@info.uas.edu.mx>
2024-12-15 20:19:18 -0700

    Add files via upload

---
+++ database.php
@@ -0,0 +1,80 @@
+<?php
+class Database
+{        
+    private $_connection;
+    private static $_instance;
+    private $_host = "dpg-ctfjm20gph6c73fn9ja0-a.oregon-postgres.render.com";
+    private $_username = "postgresql";
+    private $_password = "EWqkKLNRDDw7t3xml33xvwIWVFn1yDo4";
+    private $_database = "emirdb";
+
+    public static function getInstance()
+    {
+        if (!self::$_instance) { 
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public function __construct()
+    {
+        $this->_connection = new mysqli($this->_host, $this->_username, $this->_password, $this->_database);
+        if (mysqli_connect_error()) {
+            trigger_error("Failed to connect to MySQL: " . mysqli_connect_error(), E_USER_ERROR);
+        }
+    }
+
+    public function __clone() { }
+
+    public function getConnection()
+    {
+        return $this->_connection;
+    }
+     
+    public function get_data($sql)
+    {
+        $ret = array('STATUS'=>'ERROR','ERROR'=>'','DATA'=>array());
+        $mysqli = $this->getConnection();
+        $res = $mysqli->query($sql);
+
+        if ($res) {
+            $ret['STATUS'] = "OK";
+            while ($row = $res->fetch_array(MYSQLI_ASSOC)) {
+                $ret['DATA'][] = $row;
+            }
+        } else {
+            $ret['ERROR'] = mysqli_error($mysqli);
+        }
+
+        return $ret;
+    }
+     
+    public function exec($sql)
+    {
+        $ret = array('STATUS'=>'ERROR','ERROR'=>'');
+        $mysqli = $this->getConnection();
+        $res = $mysqli->query($sql);
+
+        if ($res)
+            $ret['STATUS'] = "OK";
+        else
+            $ret['ERROR'] = mysqli_error();
+
+        return $ret;
+    }
+
+    // Métodos para actualizar y eliminar registros
+    public function update($id, $nombre, $apellido, $email, $telefono, $edad)
+    {
+        $sql = "UPDATE Alumnos SET Nombre = '$nombre', Apellido = '$apellido', Email = '$email', Telefono = '$telefono', Edad = '$edad' WHERE id = $id";
+        return $this->exec($sql);
+    }
+
+    public function delete($id)
+    {
+        $sql = "DELETE FROM Alumnos WHERE id = $id";
+        return $this->exec($sql);
+    }
+}
+?>
+
